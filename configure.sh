70e1426511ea161e23d06794ad54cb4d4fcb580d
Javier Matusevich <javiermatusevich@gmail.com>
2015-09-12 18:27:04 -0300

    Fixed configure.sh for websites

--- configure.sh
+++ configure.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #configuring the system
-wget https://raw.githubusercontent.com/sf-wind/meteor-velocity-travis-ci/master/Makefile
+wget https://raw.githubusercontent.com/AXV-Against-Extreme-Violence/meteor-velocity-travis-ci/master/Makefile
 
 #install meteor
 curl https://install.meteor.com | /bin/sh
